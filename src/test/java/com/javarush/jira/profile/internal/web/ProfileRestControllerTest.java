f1601a94e494f9b9f985a93c07f69dbc8fe9d7df
kohmab <kohmab@gmail.com>
2024-10-26 17:14:35 +0300

    save before go home

--- src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
+++ src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
@@ -1,8 +1,17 @@
 package com.javarush.jira.profile.internal.web;
 
 import com.javarush.jira.AbstractControllerTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
 
+
+import static com.javarush.jira.profile.internal.web.ProfileTestData.*;
 
 class ProfileRestControllerTest extends AbstractControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
 
 }
